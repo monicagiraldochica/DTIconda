e91e3d9235e00495de93dd80bcb3e521aec3d8be
monicagiraldochica <monicagiraldochica@gmail.com>
2025-07-14 12:16:05 -0500

    edits

--- createEnv.sh
+++ createEnv.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 module load miniforge
-conda create -c https://fsl.fmrib.ox.ac.uk/fsldownloads/fslconda/public/ -c conda-forge -n DTIconda
+#conda create -c https://fsl.fmrib.ox.ac.uk/fsldownloads/fslconda/public/ -c conda-forge -n DTIconda
 conda activate DTIconda
-conda config --set channel_priority disabled
-conda install anaconda::libgcc
+#conda config --set channel_priority disabled
+#conda install anaconda::libgcc
 
 # Install FSL
-wget https://fsl.fmrib.ox.ac.uk/fsldownloads/fslconda/releases/fsl-6.0.7_linux-64.yml
+#wget https://fsl.fmrib.ox.ac.uk/fsldownloads/fslconda/releases/fsl-6.0.7_linux-64.yml
 f=$(python3 readFSLyml.py)
 for line in $f
 do
@@ -20,5 +20,29 @@ do
 
 	echo "Installing ${array[0]} (no specified version)..."
 	conda install ${array[0]} -y &> /dev/null
-	[ $n -gt 0 ] && echo "DONE" || echo "NOT installed"
+	if [ $n -gt 0 ]
+	then
+		echo "DONE"
+	else
+		echo "NOT installed"
+		echo $line >> NOTinstalled.txt
+	fi
 done
+
+# Install Freesurfer
+#conda install hcc::freesurfer
+
+# Install ANTS
+#conda install conda-forge::ants
+
+# Install dcmtk
+#conda install bioconda::dcmtk
+
+# Install dcm2niix
+#conda install conda-forge::dcm2niix
+
+# Install Connectome Workbench
+#conda install bioconda::connectome-workbench
+
+# Install MRtrix3
+#conda install mrtrix3::mrtrix3
